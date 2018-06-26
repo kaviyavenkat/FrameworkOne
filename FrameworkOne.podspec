1b24423761170f95d6e3690ee92b97174c71fff2
muruganandam <muruganandam@PSSHSRLT020.local>
2018-06-26 12:30:53 +0530

    Initial commit

---
+++ FrameworkOne.podspec
@@ -0,0 +1,11 @@
+Pod::Spec.new do |s|
+  s.name         = "FrameworkOne"
+  s.version      = "0.0.1"
+  s.summary      = "A short description of FrameworkOne."
+  s.homepage     = "https://github.com/kaviyavenkat/FrameworkOne"
+  s.license      = "MIT"
+  s.author             = { "kaviya" => "vkaviya96@gmail.com" }
+   s.platform     = :ios, "10.13.3"
+  s.source       = { :git => "https://github.com/kaviyavenkat/FrameworkOne.git", :tag => "0.0.1" }
+ s.source_files  = "FrameworkOne", "FrameworkOne/**/*.{h,m,swift}"  
+end
---
+++ FrameworkOne/FrameworkOne.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkOne.h
+//  FrameworkOne
+//
+//  Created by muruganandam on 26/06/18.
+//  Copyright © 2018 com.prematix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkOne.
+FOUNDATION_EXPORT double FrameworkOneVersionNumber;
+
+//! Project version string for FrameworkOne.
+FOUNDATION_EXPORT const unsigned char FrameworkOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkOne/PublicHeader.h>
+
+
